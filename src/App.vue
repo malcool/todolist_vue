a29734893869c96cea3f20a90ddcdc197e61c7f0
Malcolm Remple <hello@malcolmremple.com>
2020-03-10 19:18:41 -0400

    reset styles

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,14 @@ export default {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
 </style>

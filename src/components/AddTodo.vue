b4b531ad6ac10230cc7c4fbd23ad174212d0eeba
Malcolm Remple <hello@malcolmremple.com>
2020-03-10 23:40:21 -0400

    working on addTodo

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,17 +1,38 @@
 <template>
-    <div>
-        <form>
-            <input type="text" name="title" placeholder="Add Todo...">
-            <input type="submit" value="Submit" class="btn">
-        </form>
-    </div>
+  <div>
+      <form v-on:submit="addTodo">
+        <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+        <input type="submit" value="Submit" class="btn">
+      </form>
+  </div>
 </template>
 
 <script>
+import uuid from 'uuid';
 export default {
-    name: "AddTodo"
+	name: "AddTodo",
+	data() {
+		return {
+			title: ''
+		}
+	},
+	methods: {
+		addTodo(e) {
+			e.preventDefault();
+			const newTodo = {
+				id: uuid.v4(),
+				title: this.title,
+				completed: false
+			}
+			// send to parent 
+			this.$emit('add-todo', newTodo);
+
+			this.title = '';
+		}
+	}
 }
 </script>
+
 
 <style scoped>
   form {

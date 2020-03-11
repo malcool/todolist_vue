d94e20605e030452db55a7ce2e121a5ca8644889
Malcolm Remple <hello@malcolmremple.com>
2020-03-10 22:10:06 -0400

    added toto Delete method

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,6 +3,7 @@
     <p>
 			<input type="checkbox" v-on:change="markComplete">
       {{todo.title}}
+			<button v-on:click="$emit('del-todo', todo.id)" class="del">x</button>
     </p>
   </div>
 </template>
@@ -13,7 +14,7 @@ export default {
   props: ["todo"],
   methods: {
     markComplete() {
-      console.log(123);
+      this.todo.completed = !this.todo.completed;
     }
   }
 }

80e9481b9f31f9da99cfa1153f41db01c21a796b
Malcolm Remple <hello@malcolmremple.com>
2020-03-10 20:21:24 -0400

    added Todos component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+   <Todos />
   </div>
 </template>
 
 <script>
-
+import Todos from './components/Todos';
 export default {
   name: 'App',
-  components: {
-    
+  components: { 
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Thress",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
+ 
